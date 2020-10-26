ac1e6f223d3790f7556cca7d9746c3bc5fa9c02f
Sai Shubham <saishubhamray@gmail.com>
2020-10-26 20:20:36 +0530

    Just Tried Some Questions of Insertion

---
+++ Practice/PrQu1.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node{
+    int Data;
+    struct Node * Next;
+}Node;
+void Traversal(Node *ptr, int size){
+    for(int i = 0; i < size; i++){
+        printf("Element %d: %d \n",i+1 ,*ptr);
+        ptr = ptr->Next;
+    }
+}
+int main(){
+    
+    Node *Head;
+    Node *First;
+    Node *Second;
+
+    Head = (struct Node *) malloc(sizeof(struct Node));
+    First = (struct Node *) malloc(sizeof(struct Node));
+    Second = (struct Node *) malloc(sizeof(struct Node));
+
+    printf("Enter the First Element: ");
+    scanf("%d", &Head->Data);
+    Head->Next = First;
+    
+    printf("Enter the Second Element: ");
+    scanf("%d", &First->Data);
+    First->Next = Second;
+    
+    printf("Enter the Third Element: ");
+    scanf("%d", &Second->Data);
+    Second->Next = NULL;
+    
+    Traversal(Head, 3);
+    
+    return 0;
+}
