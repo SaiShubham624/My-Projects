3e20bafc74fbd4c2a85e7e3d0a3ef717de2a0170
Sai Shubham <saishubhamray@gmail.com>
2020-10-31 11:15:52 +0530

    Started Learning Advanced Pointer Concept in C++

--- C++/C++/Pointers.cpp
+++ C++/C++/Pointers.cpp
@@ -1,14 +1,25 @@
 #include<iostream>
 
 using namespace std;
-class Complex{
-    private:
-        
+class Complex {
+private:
+    int Real, Imaginary;
+public:
+    void SetData(int A) {
+        Real = A;
+    }
+    void GetData() {
+        Imaginary = Real*45/2+345;
+        cout << "The Real Number is " << Real << endl;
+        cout << "The Imaginary Number is " << Imaginary << endl;
+    }
 };
 int main()
 {
-    
-    Complex C;
-    
+
+    Complex* C = new Complex;
+    C->SetData(2);
+    C->GetData();
+
     return 0;
 }
