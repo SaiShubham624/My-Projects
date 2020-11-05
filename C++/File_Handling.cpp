b8151f0626187cf0de9d283a2f065e4495174451
Sai Shubham <saishubhamray@gmail.com>
2020-11-05 15:20:06 +0530

    Learned File Handling in C++

---
+++ C++/File_Handling.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+    // // Writing to a File
+    // string Name = "Sai Shubham Ray";
+    // ofstream out("Sample.txt");
+    // out<<"Hello This is the Best File I\\O" << endl << "Ok This is Amazin " + Name;
+    // out.close();
+    
+    // // Reading from a File
+    // string Name2;
+    // ifstream in("Sample.txt");
+    // getline(in, Name2);
+    // cout << Name2;
+
+    
+    return 0;
+}
