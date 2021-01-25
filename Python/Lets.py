53c09639a464910d999b4214491548ccf796d647
Sai Shubham <saishubhamray@gmail.com>
2021-01-25 20:32:33 +0530

    Deleted Everything in My Python Files for a New Start

--- Python/Lets.py
+++ Python/Lets.py
@@ -1,9 +1,1 @@
-
-print("Hi This is Sai and I am learning Python")
-Name = input("Enter your Name: ")
-Age = int(input("Enter your Age: "))
-Study = input("Enter the Name of your School\\College: ")
-
-print("Ok So Your Name is " + Name)
-print("Your Age is " + Age)
-print("You Study in " + Study)
+# I don't have any Ideas!
---
+++ Python/Main.py
@@ -0,0 +1,11 @@
+from typing import overload
+
+
+print("Binary Converter!")
+Num = int(input('Enter the Number: '))
+Binary = str(bin(Num))
+print(Binary[2:])
+
+
+
+
